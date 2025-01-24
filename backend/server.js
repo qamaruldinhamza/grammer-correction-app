a1e368fd5d6a34922b9f416fd1f7d37df64d0b20
qamaruldinhamza <qamaruldinhamza@gmail.com>
2025-01-24 14:05:36 +0500

    routes created

--- backend/server.js
+++ backend/server.js
@@ -11,5 +11,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+// Routes
+app.use("/auth", authRoutes);
+app.use("/grammar", grammarRoutes);
+
 const PORT = 5000; 
 app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
