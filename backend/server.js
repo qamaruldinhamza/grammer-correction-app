1536355b3725b9641761f50bd2377f4b005331d6
qamaruldinhamza <qamaruldinhamza@gmail.com>
2025-01-24 14:04:57 +0500

    basic backend setup

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+
+const authRoutes = require("./routes/auth");
+const grammarRoutes = require("./routes/grammar");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const PORT = 5000; 
+app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
