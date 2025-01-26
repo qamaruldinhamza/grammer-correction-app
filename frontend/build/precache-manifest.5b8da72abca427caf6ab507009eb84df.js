9fd1dbcb9b794664d6f048fd7533f56bdea00801
qamaruldinhamza <qamaruldinhamza@gmail.com>
2025-01-26 13:54:29 +0500

    frontend build

---
+++ frontend/build/precache-manifest.5b8da72abca427caf6ab507009eb84df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21ea156bbd43560e89e0e314b606f5e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83e6a5ec0a38ddd849c2",
+    "url": "/static/css/2.dbb74888.chunk.css"
+  },
+  {
+    "revision": "429fac32f858a6ca76ef",
+    "url": "/static/css/main.4b78fef3.chunk.css"
+  },
+  {
+    "revision": "83e6a5ec0a38ddd849c2",
+    "url": "/static/js/2.a2296d74.chunk.js"
+  },
+  {
+    "revision": "429fac32f858a6ca76ef",
+    "url": "/static/js/main.8058862b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
